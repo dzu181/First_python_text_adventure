29447b366f50c0c87cd88b07f7399838d25d2667
Duong Pham <duongphamhn97@gmail.com>
2023-05-21 10:11:42 +0700

    optimize

--- ex36 - without is_game_continue.py
+++ ex36 - without is_game_continue.py
@@ -227,13 +227,14 @@ def display_text(*args: any, end: str = '\n') -> None:
 	Prints given texts, character by character with a delay between each character.
 	"""
 	DELAY: float = 0.05
-	for i in range(len(args)):
+	len_args: int = len(args)
+	for i in range(len_args):
 		text: str = str(args[i])
 		for char in text:
 			print(char, end='')
 			stdout.flush() 
 			time.sleep(DELAY)
-		if (i != len(args) - 1):
+		if (i != len_args - 1):
 			print(' ', end='')
 	print(end=end)
 
