f06feb99f26b63911e166c4e9242a1ff0c5fd587
Duong Pham <duongphamhn97@gmail.com>
2023-05-25 19:04:59 +0700

    improve display and add hidden choice

--- ex36 - without is_game_continue.py
+++ ex36 - without is_game_continue.py
@@ -24,7 +24,7 @@ def start():
 # The Chest zone
 def chest():
     displayer.display_text("You see a shiny chest lying near a bush ahead.\n" +
-                           "Who knows what valuable stuff it would contain?")
+                           "Who knows what valuable stuff it would contain?\n")
 
     chest_key_obtained = False
 
@@ -35,7 +35,7 @@ def chest():
 
         if chest_choice == "1":
             displayer.display_text("You look carefully at the lock area of the chest. It is rusted badly.\n" +
-                                   "And very FRAGILE.")
+                                   "And very FRAGILE.\n")
         elif chest_choice == "2":
             chest_key_obtained = open_chest()
         elif chest_choice == "3":
@@ -46,7 +46,7 @@ def chest():
 
 
 def open_chest():
-    displayer.display_text("Alright! Let's open this chest.")
+    displayer.display_text("Alright! Let's open this chest.\n")
 
     while True:
         show_open_chest_menu()
@@ -61,20 +61,20 @@ def open_chest():
 
         elif open_choice == "2":
             displayer.display_text("Avada Kedavra!!\n" +
-                                   "* The chest remains intact. And your smile disappeared.")
+                                   "* The chest remains intact. And your smile disappeared.\n")
 
         elif open_choice == "3":
             displayer.display_text("Quit too soon?")
             dead("You died of giving up your purpose.")
         else:
-            displayer.display_text("You look around. The peaceful atmosphere calms you down.")
+            displayer.display_text("You look around. The peaceful atmosphere calms you down.\n")
 
 
 # The Crow zone
 def crow():
     displayer.display_text("After leaving the chest, you continue to the path ahead.\n" +
                            "You see a strange short tree, with a white crow on the longest branch.\n" +
-                           "The crow is holding a shiny key in its mouth.")
+                           "The crow is holding a shiny key in its mouth.\n")
 
     # Number of Failed attempts (max: 1)
     crow_false_attempt = 0
@@ -98,7 +98,7 @@ def crow():
         elif crow_choice == "3":
             displayer.display_text("The crow rushes into your hand to grab the purple diamond.\n" +
                                    "He flies away to the sky, left the shiny key fall to the grass.\n" +
-                                   "You pick up the key.\n\n")
+                                   "You pick up the key.\n")
             return True
 
 
@@ -113,6 +113,7 @@ def great_wall(chest_key, crow_key):
         displayer.display_text("-> You have the key from the shiny chest.")
     if crow_key:
         displayer.display_text("-> You have the key from the white crow.")
+    displayer.display_text()  # Empty line
 
     # A surprise later - hidden choice
     wall_attempt = 0
@@ -125,64 +126,66 @@ def great_wall(chest_key, crow_key):
         if great_wall_choice == "1":
             if chest_key:
                 displayer.display_text("You tried the Chest key, but the door won't open.\n" +
-                                       "Try something else.")
+                                       "Try something else.\n")
             else:
                 displayer.display_text("You don't have the Chest Key to use. So sad.\n" +
-                                       "Try something else.")
+                                       "Try something else.\n")
         elif great_wall_choice == "2":
             if crow_key:
-                displayer.display_text("You tried the Crow key, and the door is opened.")
+                displayer.display_text("You tried the Crow key, and the door is opened.\n")
                 good_ending()
             else:
                 displayer.display_text("You don't have the Crow Key! Remember something at the tree?\n" +
-                                       "Try something else.")
+                                       "Try something else.\n")
         elif great_wall_choice == "3":
             displayer.display_text("You cry like a baby, your guts hate you.\n" +
-                                   "But no one can help you now. Better be back.")
+                                   "But no one can help you now. Better be back.\n")
 
         wall_attempt += 1
 
         # Player rage mode. Destroy things!	(wall_attempt == 3)
         if wall_attempt >= max_wall_attempt:
-            displayer.display_text("Damn it! Here goes nothing!")
+            displayer.display_text("Damn it! Here goes nothing!\n")
             kick_door()
 
 
 def kick_door():
-    ## TODO list:
-    # Make this a 100 HP door to kick until open, and win the game.
     displayer.display_text("You got super angry!! You tried anything but the door won't open!\n" +
-                           "You decide to stomp your feet towards the door until it cracks.")
+                           "You decide to stomp your feet towards the door until it cracks.\n")
 
+    # Make this a 100 HP door to kick until open, and win the game.
     wall_hp = 100
     while wall_hp > 0:
         show_kick_door_menu(wall_hp)
         kicking = InputHandler.input("> ").strip()
         if kicking == "1":
             displayer.display_text("Brahmmm!! The door vibrates strongly from your kick.\n" +
-                                   "You have no idea you have this strength in you. The rage.")
+                                   "You have no idea you have this strength in you. The rage.\n")
             wall_hp -= 1
         elif kicking == "2":
-            displayer.display_text("You enter another dimension, where nothing moves and awares.")
+            displayer.display_text("You enter another dimension, where nothing moves and awares.\n")
             webbrowser.open('https://bit.ly/41Wp0v8')
-            input()  # Wait for user back to the game
-            displayer.display_text("Then, you are pushed back to your reality, in front of the Great Wall.\n" +
-                                   "What the...?!")
-            return  # Return to while loop in great_wall(), reset wall's HP
+            hidden_choice: str = input().strip()  # Wait for user back to the game, also an Easter egg
+            if "☌⟟⎐⟒ ⎍⌿" == hidden_choice:
+                wall_hp = 0
+            else:
+                displayer.display_text("Then, you are pushed back to your reality, in front of the Great Wall.\n" +
+                                       "What the...?!\n")
+                return  # Return to while loop in great_wall(), reset wall's HP
         elif kicking == "3":
             displayer.display_text("It is never too late to correct our mistakes.")
             dead("Die! And reset the cycle.")
         else:
-            displayer.display_text("Fuck! What do you want me to do??!?")
+            displayer.display_text("Fuck! What do you want me to do??!?\n")
 
-    displayer.display_text("The door is brokenly open. You see a big new road ahead of you.")
+    displayer.display_text("The door is brokenly open. You see a big new road ahead of you.\n")
     good_ending()
 
 
 def first_intro():
     displayer.display_text("You don't remember anything. You are fallen.\n" +
                            "You don't know anywhere to go but the path ahead.\n" +
-                           "You begin to step forward into the forest...")
+                           "You begin to step forward into the forest...\n")
 
 
 def dead(reason):
@@ -201,7 +204,7 @@ def show_crow_menu():
     displayer.display_text("What do you do next?\n" +
                            "1. Look at the crow\n" +
                            "2. Scare the crow off.\n" +
-                           "3. Show the crow your purple diamond.")
+                           "3. Show the crow your purple diamond.\n")
 
 
 def show_open_chest_menu():
@@ -209,7 +212,7 @@ def show_open_chest_menu():
     displayer.display_text("So how will you open it?\n" +
                            "1. Kick the chest hard!\n" +
                            "2. Cast a spell to open the chest.\n" +
-                           "3. Give up.")
+                           "3. Give up.\n")
 
 
 def show_chest_menu():
@@ -217,7 +220,7 @@ def show_chest_menu():
     displayer.display_text("What do you do next?\n" +
                            "1. Examine\n" +
                            "2. Open chest\n" +
-                           "3. Leave the chest alone.")
+                           "3. Leave the chest alone.\n")
 
 
 def show_great_wall_menu():
@@ -225,7 +228,7 @@ def show_great_wall_menu():
     displayer.display_text("How do you think about this wall??\n" +
                            "1. Use Chest Key.\n" +
                            "2. Use Crow Key.\n" +
-                           "3. Shout to the sky!")
+                           "3. Shout to the sky!\n")
 
 
 def show_kick_door_menu(hp):
@@ -234,13 +237,14 @@ def show_kick_door_menu(hp):
                            "Wanna kick the damn door?\n" +
                            "1. Hell yeah!\n" +
                            "2. ☌⟟⎐⟒ ⎍⌿\n" +
-                           "3. Give up.")
+                           "3. Give up.\n")
 
 
 def good_ending():
     time.sleep(MENU_DELAY)
     displayer.display_text("A new journey awaits you ahead.")
     displayer.display_text("TO BE CONTINUED...")
+    input()  # Wait for user
     exit(0)
 
 
